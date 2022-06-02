b3ebec2307b7ee189ec14477064057d88cb9a505
thsuanwu <thsuanwu@stanford.edu>
2022-06-02 09:35:55 +0000

    notebook updates

--- cs233_gtda_hw4/models/part_classifier.py
+++ cs233_gtda_hw4/models/part_classifier.py
@@ -10,8 +10,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class PointNet(nn.Module):
-    def __init__(self, init_feat_dim=3, conv_dims=[32, 64, 64, 128, 128], student_optional_hyper_param=None):
+class part_classifier(nn.Module):
+    def __init__(self, init_feat_dim=131, n_parts=4, student_optional_hyper_param=None):
         """
         Students:
         You can make a generic function that instantiates a point-net with arbitrary hyper-parameters,
@@ -22,29 +22,16 @@ class PointNet(nn.Module):
         :param init_feat_dim: input point dimensionality (default 3 for xyz)
         :param conv_dims: output point dimensionality of each layer
         """ #batch_size, num_channels, num_points
-        super(PointNet, self).__init__()
+        super(part_classifier, self).__init__()
         self.layers = nn.Sequential(
-            nn.Conv1d(init_feat_dim, 32, 1),
-            nn.ReLU(),
-            nn.Conv1d(32, 64, 1),
-            nn.ReLU(),
-            nn.Conv1d(64, 64, 1),
-            nn.ReLU(),
-            nn.Conv1d(64, 128, 1),
-            nn.ReLU(),
-            nn.Conv1d(128, 128, 1),
-            #nn.ReLU()
+            nn.Conv1d(init_feat_dim, 64, 1),
+            nn.Conv1d(64, n_parts, 1),
         )
-        self.pool = nn.MaxPool1d(1024)
-        
-        
-    def forward(self, pointclouds):
+
+    def forward(self, x):
         """
         Run forward pass of the PointNet model on a given point cloud.
-        :param pointclouds: (B x N x 3) point cloud
+        :param pointclouds: (B x N+3) point cloud
         """
-        x = self.layers(pointclouds)
-        #print('Pointnet, before pooling:',x.shape)
-        x = self.pool(x)
-        #print('Pointnet, after pooling:',x.shape)
+        x = self.layers(x)
         return x
